627e80513a4cbcd326b6a6678453895a64ff17d6
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-05-09 19:49:35 -0400

    rename the file

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -4,8 +4,9 @@ from resources.constans import TEST_SITE_URL
 from pages.index_page import IndexPage
 
 
-class TestLogin:
+class TestCases:
 
+# Login test cases
     # Test Case wrong user login
     def test_wrong_login(self, driver, wrong_username_password):
         login_page = LoginPage(driver)
@@ -16,7 +17,7 @@ class TestLogin:
         login_page.login_user(wrong_username_password[0], wrong_username_password[1])
         login_success_page = LoginSuccessPage(driver)
         login_success_lbl = login_success_page.get_login_success_label()
-        assert "Warning: No match for E-Mail Address and/or Password." == login_success_lbl, "Test Case 1 Failed"
+        assert "Warning: No match for E-Mail Address and/or Password." == login_success_lbl, "Login Test Case 1 Failed"
 
     # Test Case correct user login
     def test_correct_login(self, driver, username_password):
@@ -28,5 +29,7 @@ class TestLogin:
         login_page.login_user(username_password[0], username_password[1])
         login_success_page = LoginSuccessPage(driver)
         login_success_lbl = login_success_page.get_login_success_label()
-        assert "My Account" == login_success_lbl, "Test Case 2 Failed"
+        assert "My Account" == login_success_lbl, "Login Test Case 2 Failed"
 
+# Search test cases
+    # Test Case wrong user login
